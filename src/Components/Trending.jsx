20e2aa276d3fedcc82246b4a3834507b4e8128fb
pratikpcodes <pratikkpcodes@gmail.com>
2024-03-17 23:47:04 +0530

    2nd  commit

--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -4,7 +4,6 @@ import Cards from "./Cards";
 import { useEffect } from "react";
 import DropDown from "./DropDown";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { set } from "react-hook-form";
 import Loader from "./Loader";
 const Trending = () => {
   const [cardData, setcardData] = useState([]);
